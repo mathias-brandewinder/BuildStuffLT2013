062066e7ac960d4fb822c8fcae8ae66d1f002ba1
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-12-12 05:45:38 -0800

    Recommender project

---
+++ Recommender/Recommender/Recommendation.fsx
@@ -0,0 +1,164 @@
+﻿(*
+0. GETTING STARTED: DATA
+
+The dataset included in the file RelentlessDev
+is a list of users, followed by the ids of people
+that user follows.
+Ex: 123,456,789 indicates that the user with Id
+123 follows 2 users, with Ids 456 and 789.
+The first line of the dataset is our root user,
+@RelentlessDev ("Leeds' most relentless .Net developer!"),
+every record afterwards is users he follows, and their friends.
+In other words, it is a 2 levels deep network.
+*)
+
+
+// TODO: run the code below.
+
+
+open System
+open System.IO
+open System.Windows.Forms
+
+// Note the usage of Set to represent Friends,
+// instead of Array or List.
+type User = { UserId: int64; Friends: int64 Set }
+
+let root = __SOURCE_DIRECTORY__
+let path = root + @"\RelentlessDev"
+
+let users = 
+    File.ReadAllLines(path)
+    |> Array.map (fun line -> line.Split(','))
+    |> Array.map (fun line -> line |> Array.map (fun x -> x |> int64))
+    |> Array.map (fun line -> { UserId = line.[0]; Friends = line.[1..] |> Set.ofArray })
+
+// We extract our root user "me", and his friends:
+let me, friends = users.[0], users.[1..]
+
+
+(*
+1. VIEWING A USER PROFILE
+
+Working with the Twitter API turned out to be
+surprisingly unpleasant, so here is a quick-and-dirty
+utility function to pop up a browser in Winforms,
+and display the profile of a user identified by
+his or her user id (an int64, denoted let x = 123L)
+*)
+
+let show (uri:string) =
+    let frm = new Form(TopMost = true, Visible = true, Width = 400, Height = 600)
+    let web = new WebBrowser(Dock = DockStyle.Fill)
+    frm.Controls.Add(web)
+    frm.Show ()
+    web.Navigate(uri)
+
+let showProfile (id:int64) =
+    sprintf "https://twitter.com/intent/user?user_id=%i" id |> show
+
+
+
+// TODO: who is hiding behind the id 25663453L? 94144339L?
+
+
+
+(*
+2. FINDING PEOPLE LIKE...
+
+As a warm-up, let's start by finding users 
+similar to a given user. In this case, we'll
+extract from RelentlessDev's friends the 5
+users most similar to him.
+We could define "similar" in a variety of ways.
+In this case, we'll start by computing the
+proportion of friends they have in common:
+similarity = common friends between A and B / total friends of A and B
+*)
+
+
+//<F# Tutorial>
+//The F# immutable Set should come in handy here:
+let someData = [ 1; 2; 2; 2; 3; 50; ]
+let someSet = someData |> Set.ofList
+
+let newSet = someSet |> Set.add 42
+let anotherSet = newSet |> Set.remove 2
+
+let set1 = [ 1 .. 5 ] |> Set.ofList
+let set2 = [ 3 .. 8 ] |> Set.ofList
+let intersection = Set.intersect set1 set2
+let union = Set.union set1 set2
+let diff = Set.difference set1 set2
+let unionMany = Set.unionMany [ someSet; set1; set2; ]
+//</F# Tutorial>
+
+
+
+// TODO: write a similarity function that takes 2 users, 
+// and evaluates their similarity, computed as a float.
+
+
+// TODO: among the direct friends of the root user,
+// find the top 5 users most similar to him.
+
+
+
+(*
+3. FINDING SUITABLE CANDIDATES TO FOLLOW
+People I might want to follow are:
+* People I don't follow already (duh!)
+* People my friends follow are a good place to start
+*)
+
+
+
+// TODO: create an array of all the friends
+// of my friends that I might want to follow.
+
+
+
+(*
+4. BASIC RECOMMENDATION
+One good place to decide who to follow
+would be to look at who most of my friends follow.
+*)
+
+
+
+// TODO: from our list of candidates,
+// find the top 10 Twitter users that
+// most of my friends follow.
+
+
+ 
+(*
+5. REFINING THE RECOMMENDATION
+One direction to improve the recommendation would be
+to think about who is most similar to me. If someone
+is similar to me, chances are, I will be interested
+in who he/she follows more than someone totally different
+from me (of course, it's not all that clear cut).
+*)
+
+//<F# Tutorial>
+// The F# immutable Map would be a convenient way
+// to store the similarity between me and all my friends.
+let map0 = 
+    Map.empty 
+    |> Map.add "Alpha" 1
+    |> Map.add "Bravo" 2
+map0.["Alpha"]
+[ "A",1; "B",2; "C",3 ] |> Map.ofSeq
+//</F# Tutorial>
+
+
+
+// TODO: create a map that associates to each
+// of my friend's Id how similar they are to me
+
+
+
+// TODO: instead of simply counting how many times
+// a candidate is followed by my friends, let's
+// do a weighted count
---
+++ Recommender/Recommender/SampleCreation.fsx
@@ -0,0 +1,90 @@
+﻿(*
+This file is strictly "for information", and not
+directly useful for the exercise; it's the script
+that was used to construct the test sample.
+*)
+
+#r @"..\packages\TweetSharp.2.3.1\lib\4.0\TweetSharp.dll"
+#r @"..\packages\TweetSharp.2.3.1\lib\4.0\Hammock.ClientProfile.dll"
+
+open System
+open System.IO
+open System.Threading
+open System.Windows.Forms
+open TweetSharp
+
+// you can get a Twitter app key here:
+// https://dev.twitter.com/apps/new
+
+let consumerKey = "YOUR KEY GOES HERE"
+let consumerSecret = "YOUR SECRET GOES HERE"
+let service = TwitterService(consumerKey, consumerSecret)
+
+let requestToken = service.GetRequestToken()
+let uri = service.GetAuthorizationUri(requestToken)
+
+let frm = new Form(TopMost = true, Visible = true, Width = 500, Height = 400)
+let web = new WebBrowser(Dock = DockStyle.Fill)
+frm.Controls.Add(web)
+frm.Show
+web.Navigate(uri)
+
+// Exchange the Request Token for an Access Token
+let verifier = "123456789" // <-- This is input into your application by your user
+let access = service.GetAccessToken(requestToken, verifier)
+
+service.AuthenticateWith(access.Token, access.TokenSecret);
+
+let friendsOf userId =
+    let options = ListFriendIdsOfOptions()
+    options.UserId <- Nullable<int64>(userId)
+    options.Count <- Nullable<int>(5000)
+    userId, (service.ListFriendIdsOf options |> Seq.toArray)
+
+let findUser userId =
+    let options = GetUserProfileForOptions()
+    options.UserId <- Nullable<int64>(userId)
+    options.IncludeEntities <- Nullable<bool>(false)
+    service.GetUserProfileFor options
+
+let flatten (friendsOf:int64*int64[]) =
+    let id, friends = friendsOf
+    let flat = [| yield id; for i in friends -> i |]
+    sprintf "%s" (String.Join(",", flat))
+
+let rateLimit resources =
+    let options = GetRateLimitStatusOptions()
+    options.Resources <- resources
+    let result = service.GetRateLimitStatus(options)
+    result.Resources.Capacity
+
+let buildSample userId =
+    [| 
+        printfn "Finding friends of %i" userId
+        let id, friends = friendsOf userId
+        yield ((id, friends) |> flatten)
+        printfn "Retrieving friends of friends"
+        for friend in friends do
+            // Need to slow this down
+            // Damn you rate limits on Twitter
+            Thread.Sleep(TimeSpan(0,1,1))
+            printfn "Finding friends of %i" friend
+            let user = findUser friend
+            let protectd = user.IsProtected
+            if (protectd.HasValue && protectd.Value = false)
+            then yield (friendsOf friend |> flatten)
+    |]
+
+let saveSample fileName data = 
+    File.WriteAllLines(@"c:/users/mathias/desktop/" + fileName, data)
+
+type User = { Id:int64; Friends: int64[] }
+
+let usersFrom (data:string[]) = 
+    data 
+    |> Array.map (fun x -> x.Split(','))
+    |> Array.map (fun line -> line |> Array.map (fun x -> int64 x))
+    |> Array.map (fun x -> { Id = x.[0]; Friends = x.[1..] })
+
+// @RelentlessDev 107460704L
+buildSample 107460704L |> saveSample "RelentlessDev"
